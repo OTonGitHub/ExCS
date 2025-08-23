bcc00f115f5f5d3f660674526a984c7c85b506c0
OTonGitHub <purple@daraknet.cc>
2025-08-23 07:59:30 +0500

    ex-3 v1,2

--- lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,32 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
+    /*
+        if minutes per layer change based on the type of lasagna,
+        then change from const to a readonly (not static readonly).
 
-    // TODO: define the 'RemainingMinutesInOven()' method
+        - const → compile-time constant, baked into IL, same for all instances.
+        - readonly → runtime constant, can vary per instance (set in constructor).
+        - static readonly → runtime constant, one value shared by entire class.
+    */
+    private const short MinutesPerLayer = 2;
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int RemainingMinutesInOven(short time)
+    {
+        return ExpectedMinutesInOven() - time;
+    }
+
+    public int PreparationTimeInMinutes(short layers)
+    {
+        return layers * MinutesPerLayer;
+    }
+
+    public int ElapsedTimeInMinutes(short layers, short minutesInOven)
+    {
+        return minutesInOven + layers * MinutesPerLayer;
+    }
 }
