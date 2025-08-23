893fde30bcb50bcb620e61ae8fa7365e094ea83e
OTonGitHub <purple@daraknet.cc>
2025-08-23 08:40:07 +0500

    ex-4 v2

--- annalyns-infiltration/AnnalynsInfiltration.cs
+++ annalyns-infiltration/AnnalynsInfiltration.cs
@@ -1,40 +1,15 @@
 static class QuestLogic
 {
     public static bool CanFastAttack(bool knightIsAwake)
-    {
-        return !knightIsAwake;
-    }
+        => !knightIsAwake;
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
-    {
-        return knightIsAwake || archerIsAwake || prisonerIsAwake;
-    }
+        => knightIsAwake || archerIsAwake || prisonerIsAwake;
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
-    {
-        return !archerIsAwake && prisonerIsAwake;
-    }
+        => !archerIsAwake && prisonerIsAwake;
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake,
         bool petDogIsPresent)
-    {
-        return (
-            (petDogIsPresent && !archerIsAwake) ||
-            (prisonerIsAwake && !archerIsAwake && !knightIsAwake)
-        );
-
-        /*
-            if (petDogIsPresent)
-            {
-                return !archerIsAwake;
-            }
-
-            if (prisonerIsAwake)
-            {
-                return !archerIsAwake && !knightIsAwake;
-            }
-
-            return false;
-        */
-    }
+        => !archerIsAwake && (petDogIsPresent || !knightIsAwake && prisonerIsAwake);
 }
